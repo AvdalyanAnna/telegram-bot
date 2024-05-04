d5e11227c19d97e78216cf25cde29e4a08a1dba8
Anna Avdalyan <anna_avdalyan@intdevels.com>
2024-05-04 12:35:01 +0400

    first commit

---
+++ app.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+useHead({
+  script: [
+    {
+      src:'https://telegram.org/js/telegram-web-app.js',
+    }
+  ]
+})
+
+const tg = window?.Telegram.WebApp
+
+
+</script>
+
+<template>
+  <div>
+    <pre>{{ tg.initDataUnsafe}}</pre>
+  </div>
+</template>
